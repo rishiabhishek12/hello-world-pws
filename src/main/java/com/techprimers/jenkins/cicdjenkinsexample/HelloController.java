37ae5cbb9f62056f77aa2150a4e0057db8d6dfec
Praful Dhabekar <praful.dhabekar@vncdigital.com>
2020-03-03 11:18:36 +0530

    update

--- src/main/java/com/techprimers/jenkins/cicdjenkinsexample/HelloController.java
+++ src/main/java/com/techprimers/jenkins/cicdjenkinsexample/HelloController.java
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String hello(){ return "Hello VNC"; }
+    public String hello(){ return "Hello Gorouv"; }
 }
